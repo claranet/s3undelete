75f4f659a6e93f5db25301c487ec2f41dc8c1d70
Dan Markhasin <minimizer@gmail.com>
2022-06-30 15:49:04 +0300

    Added additional flags

--- cmd/s3undelete/main.go
+++ cmd/s3undelete/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"flag"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"log"
 	"os"
-	"s3undelete/pkg/s3undelete"
 	"strings"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"s3undelete/pkg/s3undelete"
 )
 
 func main() {
 	var bucket = flag.String("bucket", "", "Target S3 bucket (required)")
 	var keys = flag.Int64("keys", 1024, "Maximum number of keys per request")
 	var age = flag.Duration("age", time.Hour, "Maximum time since deletion")
+	var execute = flag.Bool("execute", false, "If true, executes the undelete operation. Otherwise, a dry run is performed.")
+	var prefix = flag.String("prefix", "", "S3 Key Prefix")
+	var filter = flag.String("filter", "", "A regex expression to filter the files by")
 
 	flag.Parse()
 
@@ -23,10 +27,10 @@ func main() {
 		os.Exit(2)
 	}
 
-	if err := s3undelete.Undelete(*bucket, *keys, *age); err != nil {
+	if err := s3undelete.Undelete(*bucket, *keys, *age, *execute, *prefix, *filter); err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			log.Fatalf("undelete bucket %s failed: %s", *bucket, awsErr.Message())
-    	} else {
+		} else {
 			log.Fatalf("undelete bucket %s failed: %v", *bucket, err)
 		}
 	}
